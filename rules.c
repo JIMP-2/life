4e901598b5f5e21863ec7a0e8522c4405ad99717
jakubowm <01153052@pw.edu.pl>
2020-03-06 14:31:37 +0100

    rozdzielenie pliku rules.c na funkcje

--- rules.c
+++ rules.c
@@ -3,38 +3,7 @@
 #include <string.h>
 
 
-int main(int argc, char ** argv) {
-//alokacja i wpisanie danych do tablicy dwuwymiarowej
-	//powinien byc warunek sprawdzajacy argumenty, na razie zakladamy, ze jest poprawnie i nie ma mazania po pamieci
-	int row = atoi(argv[1]);	
-	int column= atoi(argv[2]);
-	int elem=3;
-	int count=0;
-	
-	int **tab = (int**)malloc(row*sizeof(int*));
-	for(int i=0; i<row; i++) {
-		tab[i]=(int*)malloc(column*sizeof(int));
-	}
-
-	for(int i=0; i<row; i++) {
-		for(int j=0; j<column; j++) {
-			tab[i][j] = atoi(argv[elem++]);
-		}
-	}
-
-	for(int i=0; i<row; i++) {
-		for(int j=0; j<column; j++) {
-			printf("%d ", tab[i][j]);
-		}
-		printf("\n");
-	}
-	printf("\n");
-	//alokuje druga  tablice tab
-	int **copy = (int**)malloc((row+1)*sizeof(int*));
-        for(int i=0; i<(row+1); i++) {
-                copy[i]=(int*)malloc((column+1)*sizeof(int));
-        }
-
+int* zmien_stan(int* tab, int *copy, int row, int column) {
 //przejscia tab->copy
 	//liczy liczbe sasiadow
 	for(int i=0; i<row; i++) {
@@ -69,6 +38,8 @@ int main(int argc, char ** argv) {
 			count=0;
 		}
 	}
+	return copy;
+}
 
 /*	for(int i=0; i<row; i++) {
                 for(int j=0; j<column; j++) {
@@ -77,6 +48,8 @@ int main(int argc, char ** argv) {
                 printf("\n");
         }
 */
+
+int* copy_tab(int*tab, int row, int column) {
 	//kopiuje zmieniona tablice copy->tab
 	for(int i=0; i<row; i++) {
                 memcpy(&tab[i], &copy[i], sizeof(tab[0]));
@@ -88,20 +61,7 @@ int main(int argc, char ** argv) {
                 }
                 printf("\n");
         }
-
-
-	//zwalnianie pamieci
-	for(int i=0; i<column; i++) {
-		free(tab[i]);
-	}
-	free(tab);
-
-	for(int i=0; i<column; i++) {
-                free(copy[i]);
-        }
-        free(copy);
-
-
-	return 0;
+	return copy;
 }
+
 
