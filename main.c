7831f0831582c9961576624140fd1b97891f21cb
jakubowm <01153052@pw.edu.pl>
2020-03-07 16:39:05 +0100

    poprawa kodu main.c, dodanie funkcji z tab2d.h

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include"rules.h"
 #include<string.h>
+
+#include"rules.h"
+#include"tab2d.h"
 
 
 int main ( int argc, char *argv[]){
@@ -9,47 +11,27 @@ int main ( int argc, char *argv[]){
     	FILE *in = argc > 1 ? fopen( argv[1], "r" ) : stdin;
         FILE *out = argc > 2 ? fopen( argv[2], "w" ) : stdout;
    
-    int x;
-    int y;
+	int x;
+    	int y;
 
-          fscanf(in,"%d %d",&x,&y); 
+       	fscanf(in,"%d %d",&x,&y); 
 
+	printf("%d %d \n", x,y);
+	
+	int **t = alokuj2d(x,y);
 
-printf(" %d %d \n", x,y);
+	for (int  i = 0; i < x; i++ ) {
+        	for ( int j = 0; j < y; j++ ) 
+           	fscanf(in,"%d", &t[i][j] ); 
+  	}
+	
+	fclose(in);	
 
-           int **t; 
-    t = malloc( x* sizeof( int * ) ); 
-
-     if ( t == NULL ) 
-    { 
-
-        printf("Brak pamieci!\n"); 
-        return 1;
-    } 
-    else{
-
-    for ( int i = 0; i < x; i++ ) 
-    { 
-        t[i] = malloc( y* sizeof( int ) ); 
-        if ( t[i] == NULL ) 
-        { 
-            printf("brak pamieci!\n"); 
-            return -1;
-        } 
-    } 
-    for (int  i = 0; i < x; i++ ) {
-        for ( int j = 0; j < y; j++ ) 
-           fscanf(in,"%d", &t[i][j] ); 
-    }
-
-	int **copy = (int**)malloc((x+1)*sizeof(int*));
-        for(int i=0; i<(x+1); i++) {
-                copy[i]=(int*)malloc((y+1)*sizeof(int));
-        }
+    	int **copy = alokuj2d(x,y);
 
 
-zmien_stan(t,copy,x,y);
-copy_tab(t,x,y);
+	zmien_stan(t,copy,x,y);
+	copy_tab(t,copy,x,y);
 
 
 // zapis do pliku
@@ -65,22 +47,12 @@ copy_tab(t,x,y);
 
 	fclose( out );
 
-    }
-
 
 	//zwalnianie pamieci
-	for(int i=0; i<x; i++) {
-		free(t[i]);
-	}
-	free(t);
-
-	for(int i=0; i<y; i++) {
-                free(copy[i]);
-        }
-        free(copy);
-
+	zwolnij2d(t, x);
+	zwolnij2d(copy,x);
 
 	return 0;
 
-    return 0 ; 
+     
 } 
